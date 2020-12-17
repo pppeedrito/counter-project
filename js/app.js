69d8561699c1757b589d973bc969e319b9c3c07c
pedro nin <pedro1996616@gmail.com>
2020-12-17 13:20:12 +0100

    resuelto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 // Tu código aquí
+const labelNum = document.querySelector("#counter");
+const btnmenos = document.querySelector("#btnmenos");
+//const btnmas = docuemnt.querySelector("#btnmas");
+var contador = 0;
+btnmas.addEventListener("click", function(){
+     contador = contador +1;
+  if(contador == 0){
+        labelNum.style.color ="black"
+    }else if (contador <0){
+        labelNum.style.color ="red"
+    }else if (contador > 0){
+        labelNum.style.color ="green"
+    }
+
+labelNum.textContent = contador;
+})
+
+btnmenos.addEventListener("click", function(){
+    contador = contador -1;
+    if(contador == 0){
+        labelNum.style.color ="black"
+    }else if (contador <0){
+        labelNum.style.color ="red"
+    }else if (contador > 0){
+        labelNum.style.color ="green"
+    }
+  
+    labelNum.textContent = contador;
+})
